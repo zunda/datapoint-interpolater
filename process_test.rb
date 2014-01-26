c064ea2ca1d13044e41571a442af5ffaa7e395bc
zunda <zundan@gmail.com>
2014-01-26 13:46:03 -1000

    Add test on 1D, remove extra assert from 2D

--- process_test.rb
+++ process_test.rb
@@ -7,13 +7,20 @@ $:.unshift(File.expand_path(File.dirname(__FILE__)))
 require 'process'
 
 class TestGridScanner < Test::Unit::TestCase
+	def test_scan_1d
+		result = Array.new
+		GridScanner.new(1).scan do |delta|
+			result << delta
+		end
+		assert_equal([[-1],[0],[1]].sort, result.sort)
+	end
+
 	def test_scan_2d
 		target = [[-1,-1],[0,-1],[1,-1],[-1,0],[0,0],[1,0],[-1,1],[0,1],[1,1]]
 		result = Array.new
 		GridScanner.new(2).scan do |delta|
 			result << delta
 		end
-		assert_equal(target.size, result.size)
 		assert_equal(target.sort, result.sort)
 	end
 end
